3d9b1e1554d88f6e405eca16220bc6adedde8a6b
t2-millssmith <Thomas465@live.co.uk>
2016-10-19 17:45:58 +0100

    Added noise debugging button (spacebar) and some other small fixes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager gameManagerSingleton;
+
+    public enum GameStates { TITLESCREEN, PLAYING, FINISHINGLEVEL, GAMEOVER, LEADERBOARD }
+    public GameStates currentGameState;
+
+    public static int currentStage = 0;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (!gameManagerSingleton)
+        {
+            gameManagerSingleton = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeGameState(GameStates newGameState)
+    {
+        Debug.Log("Game state is now " + newGameState + "!");
+        currentGameState = newGameState;
+    }
+
+    public GameStates GetCurrentGameState()
+    {
+        return currentGameState;
+    }
+}
