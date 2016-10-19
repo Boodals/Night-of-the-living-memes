e5d5c21c8b17ec642037657adeb144282c73460b
UWETom123 <tom.moreton@yahoo.co.uk>
2016-10-19 18:35:53 +0100

    Memescreen online

---
+++ Assets/Scripts/MemeManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemeManager : MonoBehaviour {
+
+    public GameObject plane;
+    Material memeImage;
+    int randomArrayIndex;
+    int memeAmount = 19;
+
+    void Start()
+    {
+        randomArrayIndex = Random.Range(0, memeAmount);
+        Material[] memeImages = Resources.LoadAll<Material>("Materials");
+        memeImage = memeImages[randomArrayIndex];
+        plane.GetComponent<Renderer>().material = memeImage;
+    }
+
+    void Update()
+    {
+
+    }
+	
+}
