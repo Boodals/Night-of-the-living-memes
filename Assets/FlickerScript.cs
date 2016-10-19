65a77c99987873e9e33a05a8e29d4a823d9e7497
t2-millssmith <Thomas465@live.co.uk>
2016-10-19 19:47:10 +0100

    Added some more detail and an unreasonable amount of screen effects

---
+++ Assets/FlickerScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickerScript : MonoBehaviour
+{
+
+    public Light myLight;
+
+    enum states { normal, flickering, off }
+    states currentState;
+
+    float timer = 4, initialIntensity;
+
+    // Use this for initialization
+    void Start()
+    {
+        initialIntensity = myLight.intensity;
+
+        SwitchState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            SwitchState();
+        }
+
+        switch (currentState)
+        {
+            case states.flickering:
+                myLight.intensity = Random.Range(0, initialIntensity*1.2f);
+                break;
+            case states.normal:
+                myLight.intensity = initialIntensity;
+                break;
+            case states.off:
+                myLight.intensity -= Time.deltaTime * 2;
+                break;
+        }
+    }
+
+    void SwitchState()
+    {
+        timer = Random.Range(3, 6);
+
+        currentState = (states)Random.Range(0, 2);
+
+        if (currentState == states.flickering)
+            timer *= 0.2f;
+    }
+}
