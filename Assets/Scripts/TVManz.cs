d710c59196866af4ed7e682ce90a58c8ba9ec699
OCHyams <Orlando2.Cazalet-Hyams@live.uwe.ac.uk>
2016-10-20 11:10:10 +0100

    Added code ready for "torch on" causing enemies to see player form further. Changed some AI behaviour parameters

--- Assets/Scripts/TVManz.cs
+++ Assets/Scripts/TVManz.cs
@@ -7,22 +7,25 @@ public class TVManz : EnemyBase
     public float m_alertListenRadius;
     public float m_chaseSpeed;
     public float m_alertTime;
+    public float m_torchOnViewboxMultiplier;
 
+    public Vector3 m_curViewbox;
     public Vector3 m_viewBoxPassive;
     public Vector3 m_viewBoxAlert;
     public Vector3 m_viewBoxChase;
 
-    public Vector3 m_anchor;
-    public Vector3 m_target;
     public float m_wanderRadius;
+    protected Vector3 m_anchor;
+    protected Vector3 m_target;
 
     protected PlayerScript m_player;
     protected NavMeshAgent m_navAgent;
     protected BoxCollider m_viewbox;
-
+    
     protected float m_defaultSpeed;
     protected float m_listenRadius;
     protected bool m_playerInViewbox;
+    [SerializeField]
     protected float m_alertTimer;
     //state stuff
     protected StateContoller m_SC;
@@ -55,6 +58,11 @@ public class TVManz : EnemyBase
     void Update()
     {
         m_SC.update();
+
+        //if (player.torchToggle && torchOn)
+        //m_viewbox.size = m_curViewbox * m_torchOnViewboxMultiplier;
+        //else if (torchtoggle && torchOff)
+        //m_viewbox.size = m_curViewbox;
     }
 
     public bool canSeePlayer()
@@ -109,6 +117,14 @@ public class TVManz : EnemyBase
         {
             m_SC.transition(PASSIVE);
         }
+        else
+        {
+            //look around Sort this out
+            //float angle = Mathf.Sin(m_alertTimer / m_alertTime) * 50;
+            //float angle = Mathf.Asin(m_alertTimer / m_alertTime) * 10;
+            //transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+
+        }
         m_alertTimer += Time.deltaTime;
     }
     
@@ -116,7 +132,7 @@ public class TVManz : EnemyBase
     protected void chasingUpdate()
     {
         m_navAgent.SetDestination(m_player.gameObject.transform.position);
-        if (!m_playerInViewbox || !canSeePlayer())
+        if (!m_playerInViewbox && !canSeePlayer())
         {
             m_SC.transition(ALERT);
         }
@@ -126,7 +142,8 @@ public class TVManz : EnemyBase
     protected void anyToAlert()
     {
         //Debug.Log("trans to alert");
-        m_viewbox.size = m_viewBoxAlert;
+        m_curViewbox = m_viewBoxAlert;
+        m_viewbox.size = m_curViewbox;
         m_listenRadius = m_alertListenRadius;
         m_navAgent.speed = m_defaultSpeed;
         m_alertTimer = 0.0f;
@@ -139,7 +156,8 @@ public class TVManz : EnemyBase
         //Debug.Log("trans to passive");
         setNewDestination();
 
-        m_viewbox.size = m_viewBoxPassive;
+        m_curViewbox = m_viewBoxPassive;
+        m_viewbox.size = m_curViewbox;
         m_listenRadius = m_passiveListenRadius;
         m_navAgent.speed = m_defaultSpeed;
     }
@@ -148,7 +166,8 @@ public class TVManz : EnemyBase
     protected void anyToChasing()
     {
         //Debug.Log("trans to chasing");
-        m_viewbox.size = m_viewBoxChase;
+        m_curViewbox = m_viewBoxChase;
+        m_viewbox.size = m_curViewbox;
         m_navAgent.speed = m_chaseSpeed;
     }
 
