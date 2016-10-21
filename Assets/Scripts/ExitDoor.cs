86a25b27dd81f946eeb07ac96f71187a8798a419
OCHyams <Orlando2.Cazalet-Hyams@live.uwe.ac.uk>
2016-10-21 10:44:43 +0100

    Oops forgot my scripts.

---
+++ Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ExitDoor : MonoBehaviour
+{
+
+    public float m_fadeDuration;
+    public float m_fadeLinger;
+    public Color m_fadeTarget;
+    public Transform m_playerStartTransform;
+    public Light m_activeLight;
+
+    private Image m_fader;
+    private Color m_startCol;
+    private StateContoller m_SC;
+    private PlayerScript m_player;
+    private float m_timer;
+
+
+    protected const short INACTIVE = 1;
+    protected const short ENTER_LEVEL = 2;
+    protected const short EXIT_LEVEL = 4;
+    protected const short ACTIVE = 8;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        m_SC = new StateContoller(this);
+        m_SC.transition(INACTIVE);
+
+        m_player = GameObject.Find("Player").GetComponent<PlayerScript>();
+        m_fader = GameObject.Find("FadeCanvas").GetComponent<Canvas>().GetComponentInChildren<Image>();
+
+        m_startCol = m_fader.color;
+    }
+
+    public void activate()
+    {
+        m_SC.transition(ACTIVE);
+    }
+
+    //just for testing, as the deactivation will probably be handled internally
+    public void deactivate()
+    {
+        m_SC.transition(INACTIVE);
+    }
+
+    [Update(INACTIVE)]
+    private void inactive()
+    {
+        //do nothin'
+    }
+
+    [Update(ACTIVE)]
+    private void active()
+    {
+        //do nothin' probably?
+    }
+
+    [Update(ENTER_LEVEL)]
+    private void enter()
+    {
+        m_fader.color = Color.Lerp(m_fadeTarget,m_startCol , m_timer / m_fadeDuration);
+        if (m_timer >= m_fadeDuration)
+        {
+            m_SC.transition(INACTIVE);
+        }
+    }
+
+    [Update(EXIT_LEVEL)]
+    private void exit()
+    {
+        m_fader.color = Color.Lerp(m_startCol, m_fadeTarget, m_timer/ m_fadeDuration);
+        if (m_timer >= m_fadeDuration + m_fadeLinger)
+        {
+            m_SC.transition(ENTER_LEVEL);
+        }
+    }
+
+    [Transition(StateContoller.ANY_STATE, INACTIVE)]
+    private void anyToInactive()
+    {
+        //close doors
+        m_activeLight.enabled = false;//fade out would look nicer
+    }
+
+    [Transition(StateContoller.ANY_STATE, ACTIVE)]
+    private void anyToActive()
+    {
+        m_activeLight.enabled = true;
+    }
+
+    [Transition(StateContoller.ANY_STATE, ENTER_LEVEL)]
+    private void anyToEnter()
+    {
+        //open the doors?
+
+        //auto pilot the player forwards a few steps?
+
+        //start fade-to-white?
+        m_player.transform.position = m_playerStartTransform.position;
+        //m_player.transform.transform.rotation = m_playerStartTransform.rotation;//ASK TMS ABOUT THIS
+        m_timer = 0.0f;
+    }
+    [Transition(StateContoller.ANY_STATE, EXIT_LEVEL)]
+    private void anyToExit()
+    {
+        //open the doors?
+
+        //player cannot move/ is moved through doors?
+
+        //start fade white-to-normal?
+
+        m_timer = 0.0f;
+
+        //reset level
+        //increase score multiplier
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        m_SC.update();
+        m_timer += Time.deltaTime;
+    }
+
+    void OnTriggerStay(Collider _other)
+    {
+        if (_other.tag == Tags.Player /*&& Input.GetButtonDown("action" )*/) 
+        {
+            m_SC.transition(EXIT_LEVEL);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/ExitManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitManager : MonoBehaviour
+{
+    public ExitDoor[] m_doors;
+    private static ExitManager s_instance;
+    private int m_curActive;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Assert(s_instance == null, "Only one ExitManger allowed!");
+        s_instance = this;
+
+        GameObject[] doors = GameObject.FindGameObjectsWithTag(Tags.Door);
+        m_doors = new ExitDoor[doors.Length];
+        for (int i = 0; i < doors.Length; ++i)
+        {
+            m_doors[i] = doors[i].GetComponent<ExitDoor>();
+        }
+        m_curActive = -1;
+    }
+
+    public static void activateRandomDoor()
+    {
+        s_instance.m_curActive = Random.Range(0, s_instance.m_doors.Length);
+        s_instance.m_doors[s_instance.m_curActive].activate();
+    }
+
+    public static void deactivateCurDoor()
+    {
+        if (s_instance.m_curActive >= 0) s_instance.m_doors[s_instance.m_curActive].deactivate();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            deactivateCurDoor();
+            activateRandomDoor();
+        }
+           
+    }
+}
