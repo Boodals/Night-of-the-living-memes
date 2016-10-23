9ed705ea226e4813bf49bce6c963f9d92785deb4
Boodals <HarrisonMaund.Boodals@gmail.com>
2016-10-23 12:45:09 +0100

    Nyan cat spawns at the furthest NyanCatSpawnPoint to the player

--- Assets/Scripts/NyanCatSpawner.cs
+++ Assets/Scripts/NyanCatSpawner.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Collections;
 
 public class NyanCatSpawner : MonoBehaviour
@@ -7,6 +8,8 @@ public class NyanCatSpawner : MonoBehaviour
 	public float timeToSpawn = 180f; //3 mins
 
 	public NyanCat nyanCat;
+
+	public PlayerScript player;
 
 
 	void Start()
@@ -19,9 +22,31 @@ public class NyanCatSpawner : MonoBehaviour
 	IEnumerator WaitForSpawn()
 	{
 		yield return new WaitForSeconds(timeToSpawn);
+		SpawnNyanCat();
+	}
+
+
+	private void SpawnNyanCat()
+	{
+		NyanCatSpawnPoint bestSpawn = null;
+		float bestDist = Mathf.NegativeInfinity;
+
+		foreach(NyanCatSpawnPoint sp in FindObjectsOfType<NyanCatSpawnPoint>())
+		{
+			float dist = (sp.transform.position - player.transform.position).sqrMagnitude;
+            if(bestDist < dist)
+			{
+				bestDist = dist;
+				bestSpawn = sp;
+			}
+		}
+
+		nyanCat.transform.position = bestSpawn.transform.position;
+		nyanCat.transform.rotation = bestSpawn.transform.rotation;
+
+		nyanCat.gameObject.SetActive(true);
 
 		Debug.Log("Spawned nyan cat");
-		nyanCat.gameObject.SetActive(true);
 	}
 
 
