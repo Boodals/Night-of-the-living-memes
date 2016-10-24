b6033cd18673cb3e4f6dfbf2b01dfb48c8b7e2f6
UWETom123 <tom.moreton@yahoo.co.uk>
2016-10-24 14:05:17 +0100

    Camera now zooms into gameover scene

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour {
+
+    float zoomSpeed = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.transform.position.z < -12.25f)
+        {
+            Debug.Log(gameObject.transform.position.z);
+            gameObject.transform.position += Vector3.forward * zoomSpeed * Time.deltaTime;
+        }
+        else
+        {
+            zoomSpeed = 0.0f;
+        }
+	
+	}
+}
